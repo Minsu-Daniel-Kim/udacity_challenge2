423f28085351ff7ad8232dc5ac1e88f6dc4a37ff
daniel <minsu.daniel.kim@gmail.com>
2016-10-25 16:32:33 -0700

    working on modeling_pipeline.py and ipython sanity_check added

--- pipeline/modeling_pipeline.py
+++ pipeline/modeling_pipeline.py
@@ -6,60 +6,65 @@ import utils
 
 def train():
 
+    LOGDIR = '../save/model'
     models = utils.from_recipe()
+
+    model_dict = {
+
+        'driveNet': DriveNet
+
+    }
 
     for model in models:
 
-        config = utils.from_json_file("config", "%s.json" % model)
-
         # config
-        NUM_ITER = config["NUM_ITER"]
-        BATCH_SIZE = config["BATCH_SIZE"]
-        MODEL_TITLE = config["MODEL_TITLE"]
-        MODEL_FILE = config["MODEL_FILE"]
-        LOGDIR = '../save/model'
-        summaries_dir = '/tmp/' + MODEL_TITLE
+        NUM_ITER = model["NUM_ITER"]
+        BATCH_SIZE = model["BATCH_SIZE"]
+        MODEL_TITLE = model["MODEL_TITLE"]
+        MODEL_FILE = model["MODEL_FILE"]
+        SUMMARY_DIR = '/tmp/' + MODEL_TITLE
 
+        # get session
         sess = tf.InteractiveSession()
 
-        model = DriveNet(width=config["WIDTH"], height=config["HEIGHT"], channel=config["CHANNEL"])
-        y, y_, x, keep_prob = model.inference()
+        # setup model
+        dnn = model_dict[model['MODEL_TITLE']](width=model["WIDTH"], height=model["HEIGHT"], channel=model["CHANNEL"])
+        dnn.inference()
 
         with tf.name_scope('loss'):
-            loss = tf.reduce_mean(tf.square(tf.sub(y_, y)))
+            loss = tf.reduce_mean(tf.square(tf.sub(dnn.y_, dnn.y)))
             tf.scalar_summary('mse', loss)
 
         train_step = tf.train.AdamOptimizer(1e-4).minimize(loss)
         sess.run(tf.initialize_all_variables())
 
+        # summary statistics
         merged = tf.merge_all_summaries()
-        train_writer = tf.train.SummaryWriter(summaries_dir + '/train',
-                                              sess.graph)
-        test_writer = tf.train.SummaryWriter(summaries_dir + '/test')
+        train_writer = tf.train.SummaryWriter(SUMMARY_DIR + '/train', sess.graph)
+        test_writer = tf.train.SummaryWriter(SUMMARY_DIR + '/test')
 
+        # saver
         saver = tf.train.Saver()
 
         for i in range(NUM_ITER):
             xs, ys = driving_data.LoadTrainBatch(BATCH_SIZE)
             if i % 10 == 0:
-                summary, acc = sess.run([merged, loss], feed_dict={x: xs, y_: ys, keep_prob: 0.8})
+                xs, ys = driving_data.LoadValBatch(100)
+                summary, acc = sess.run([merged, loss], feed_dict={dnn.x: xs, dnn.y_: ys, dnn.keep_prob: 0.8})
                 test_writer.add_summary(summary, i)
-                print('MSE at step %s: %s' % (i, acc))
-            else:  # Record train set summaries, and train
-
-                if i % 100 == 99:  # Record execution stats
-                    run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
-                    run_metadata = tf.RunMetadata()
-                    summary, _ = sess.run([merged, train_step],
-                                          feed_dict={x: xs, y_: ys, keep_prob: 0.8},
-                                          options=run_options,
-                                          run_metadata=run_metadata)
-                    train_writer.add_run_metadata(run_metadata, 'step%03d' % i)
-                    train_writer.add_summary(summary, i)
-                    print('Adding run metadata for', i)
-                else:  # Record a summary
-                    summary, _ = sess.run([merged, train_step], feed_dict={x: xs, y_: ys, keep_prob: 0.8})
-                    train_writer.add_summary(summary, i)
+            elif i % 100 == 99:  # Record execution stats
+                run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+                run_metadata = tf.RunMetadata()
+                summary, _ = sess.run([merged, train_step],
+                                      feed_dict={dnn.x: xs, dnn.y_: ys, dnn.keep_prob: 0.8},
+                                      options=run_options,
+                                      run_metadata=run_metadata)
+                train_writer.add_run_metadata(run_metadata, 'step%03d' % i)
+                train_writer.add_summary(summary, i)
+                print('Adding run metadata for', i)
+            else:
+                summary, _ = sess.run([merged, train_step], feed_dict={dnn.x: xs, dnn.y_: ys, dnn.keep_prob: 0.8})
+                train_writer.add_summary(summary, i)
             if i % 100 == 0:
                 if not os.path.exists(LOGDIR):
                     os.makedirs(LOGDIR)
@@ -76,9 +81,7 @@ def train():
         test_writer.close()
      
 def main(_):
-
     train()
-
 
 if __name__ == '__main__':
     tf.app.run()
